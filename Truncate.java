f2d71cd3d6c2d37982a4f379accd19a9dbe6316c
edersonsm <edersondesouzamarques@gmail.com>
2026-08-21 11:07:40 -0300

    Truncate

---
+++ Truncate.java
@@ -0,0 +1,14 @@
+public class Truncate {
+    public static void main(String[] args) {
+        double tax = 145.3843497374374;
+        System.out.println("tax is $" + (int)tax);
+        System.out.println("tax is $" + (int)(tax * 10) / 10.0);
+        System.out.println("tax is $" + (int)(tax * 100) / 100.0);
+        System.out.println("tax is $" + (int)(tax * 1000) / 1000.0);
+        System.out.println("tax is $" + (int)(tax * 100000) / 100000.0);
+        System.out.println("tax is $" + tax);
+        
+        
+        
+    }
+}
