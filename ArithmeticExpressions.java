b07f88126b2d4b354888ab023159e450b8da1a5b
edersonsm <edersondesouzamarques@gmail.com>
2026-08-21 10:05:54 -0300

    Arithmetic expressions

---
+++ ArithmeticExpressions.java
@@ -0,0 +1,8 @@
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+        double a = 1, b = 2, c = 3.4, d = 9, r = 1.2, t = 3.5;
+        System.out.println(4.0 / (3 * (r + 34)) - 9.0 * (a + b * c) + (3 + d * (2 + a)) / (a + b * d));
+        System.out.println(5.5 * Math.pow(r + 2.5, 2.5 + t));
+    }
+}
+
