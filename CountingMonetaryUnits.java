5587fc357e242a4c84cc15069a965cdb1a195478
edersonsm <edersondesouzamarques@gmail.com>
2026-08-23 11:35:46 -0300

    counting monetary units

---
+++ CountingMonetaryUnits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountingMonetaryUnits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the amount (5.79): ");
+        double amount = input.nextDouble();
+
+        int remainingCents = (int)(amount * 100); // Imprecision problem
+        int dollars = remainingCents / 100;
+        remainingCents %= 100;
+        int quarters = remainingCents / 25;
+        remainingCents %= 25;
+        int dimes = remainingCents / 10;
+        remainingCents %= 10;
+        int nickels = remainingCents / 5;
+        remainingCents %= 5;
+        int pennies = remainingCents;
+
+        System.out.println("\n\nNumber of dollars: " + dollars);
+        System.out.println("Number of quarters: " + quarters);
+        System.out.println("Number of dimes: " + dimes);
+        System.out.println("Number of nickels: " + nickels);
+        System.out.println("Number of pennies: " + pennies);
+    }
+}
