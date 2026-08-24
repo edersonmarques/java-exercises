c24755b541aab3199254b5fe78fcb8bcbfd8fc89
edersonsm <edersondesouzamarques@gmail.com>
2026-08-24 12:43:46 -0300

    comparison of floating point numbers

---
+++ ComparisonOfFloatingPoint.java
@@ -0,0 +1,12 @@
+public class ComparisonOfFloatingPoint {
+    public static void main(String[] args) {
+        
+        final double EPSILON = 1E-14;
+
+        double x = 1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1;
+
+        if (Math.abs(x - 0.5) < EPSILON) {
+            System.out.println(x + " is approximately 0.5");
+        }
+    }
+}
