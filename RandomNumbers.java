50dcf454aa17123863126683edca598d62747b41
edersonsm <edersondesouzamarques@gmail.com>
2026-08-24 13:39:15 -0300

    random numbers

---
+++ RandomNumbers.java
@@ -0,0 +1,6 @@
+public class RandomNumbers {
+    public static void main(String[] args) {
+        int randomInt = (int)(Math.random() * 10); // Math.random() returns a random value from 0.0 includedto 1.0 excluded
+        System.out.println(randomInt);
+    }
+}
