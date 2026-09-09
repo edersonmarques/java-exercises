7194b35184f16bc94442ed319ea01d2a4c51f642
edersonsm <edersondesouzamarques@gmail.com>
2026-09-09 18:31:54 -0300

    radians to degree

---
+++ RadiansToDegrees.java
@@ -0,0 +1,6 @@
+public class RadiansToDegrees {
+    public static void main(String[] args) {
+        double degreesAngle = Math.toDegrees(Math.PI / 7);
+        System.out.println(degreesAngle);
+    }
+}
